ce38d546018bb65caee13df39f7ed6091b4f5163
Edgar <Edgar@AnotherFoxGuy.com>
2020-10-25 12:19:59 +0100

    t

---
+++ lib/FindHumanize.cmake
@@ -0,0 +1,5 @@
+
+add_library(Humanize::Humanize INTERFACE IMPORTED)
+set_target_properties(Humanize::Humanize PROPERTIES
+        INTERFACE_INCLUDE_DIRECTORIES "${CMAKE_SOURCE_DIR}/lib/humanize/include/"
+        )
