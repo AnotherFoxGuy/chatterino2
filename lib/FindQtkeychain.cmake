dfd725893f0c637d8d74d82e901f2ccacc42f074
Edgar <Edgar@AnotherFoxGuy.com>
2020-12-07 14:11:34 +0100

    Hmmm

--- lib/FindQtkeychain.cmake
+++ lib/FindQtkeychain.cmake
@@ -1,21 +1,5 @@
 set(Qtkeychain_FOUND TRUE)
 
-ExternalProject_Add(
-        Qtkeychain
-        SOURCE_DIR ${CMAKE_SOURCE_DIR}/lib/qtkeychain
-        CMAKE_ARGS
-        -DQTKEYCHAIN_STATIC=ON
-        -DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE}
-        -DCMAKE_PREFIX_PATH=${CMAKE_PREFIX_PATH}
-        -DCMAKE_INSTALL_PREFIX=<INSTALL_DIR>
-        -DCMAKE_PREFIX_PATH=${CMAKE_PREFIX_PATH}
-)
 
-ExternalProject_Get_property(Qtkeychain INSTALL_DIR)
-file(MAKE_DIRECTORY ${INSTALL_DIR})
-
-add_library(Qtkeychain::Qtkeychain INTERFACE IMPORTED)
-set_target_properties(Qtkeychain::Qtkeychain PROPERTIES
-        INTERFACE_LINK_LIBRARIES "${INSTALL_DIR}/lib/qtkeychain.${CMAKE_STATIC_LIBRARY_SUFFIX}"
-        INTERFACE_INCLUDE_DIRECTORIES "${INSTALL_DIR}"
-        )
+add_subdirectory("${CMAKE_SOURCE_DIR}/lib/qtkeychain/" "${CMAKE_BINARY_DIR}/lib/qtkeychain/")
+#include("${CMAKE_BINARY_DIR}/lib/qtkeychain/Qt5KeychainConfig.cmake")
