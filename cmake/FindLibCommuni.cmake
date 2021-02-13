e4b4ac9f4a14e6b2a4fc4b028bbf8dbace1aebff
Rasmus Karlsson <rasmus.karlsson@pajlada.com>
2021-02-13 13:14:46 +0100

    Revert libCommuni find file changes

--- cmake/FindLibCommuni.cmake
+++ cmake/FindLibCommuni.cmake
@@ -1,16 +1,11 @@
-find_path(IrcCore_INCLUDE_DIR irc.h PATH_SUFFIXES IrcCore HINTS ${CMAKE_SOURCE_DIR}/lib/libcommuni/include)
-find_library(IrcCore_LIBRARY NAMES Core IrcCore HINTS ${CMAKE_SOURCE_DIR}/lib/libcommuni/lib)
-message(STATUS "${IrcCore_LIBRARY}")
+find_path(IrcCore_INCLUDE_DIR irc.h PATH_SUFFIXES IrcCore)
+find_library(IrcCore_LIBRARY Core)
 
-find_path(IrcModel_INCLUDE_DIR ircmodel.h PATH_SUFFIXES IrcModel HINTS ${CMAKE_SOURCE_DIR}/lib/libcommuni/include)
-find_library(IrcModel_LIBRARY NAMES Model IrcModel HINTS ${CMAKE_SOURCE_DIR}/lib/libcommuni/lib)
-message(STATUS "${IrcModel_LIBRARY}")
-message(STATUS "${IrcModel_INCLUDE_DIR}")
+find_path(IrcModel_INCLUDE_DIR IrcModel.h PATH_SUFFIXES IrcModel)
+find_library(IrcModel_LIBRARY Model)
 
-find_path(IrcUtil_INCLUDE_DIR ircutil.h PATH_SUFFIXES IrcUtil HINTS ${CMAKE_SOURCE_DIR}/lib/libcommuni/include)
-find_library(IrcUtil_LIBRARY NAMES Util IrcUtil HINTS ${CMAKE_SOURCE_DIR}/lib/libcommuni/lib)
-
-message(STATUS "${IrcUtil_INCLUDE_DIR}")
+find_path(IrcUtil_INCLUDE_DIR ircutil.h PATH_SUFFIXES IrcUtil)
+find_library(IrcUtil_LIBRARY Util)
 
 set(LibCommuni_INCLUDE_DIRS ${IrcCore_INCLUDE_DIR} ${IrcModel_INCLUDE_DIR} ${IrcUtil_INCLUDE_DIR})
 set(LibCommuni_LIBRARIES ${IrcCore_LIBRARY} ${IrcModel_LIBRARY} ${IrcUtil_LIBRARY})
